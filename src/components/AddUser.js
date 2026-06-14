66c5ab0d4bfb9ffb55745b3cf95fee67402abf7d
Ashutosh Samal <samalashutosh71@gmail.com>
2026-06-14 13:01:36 +0530

    Update API base to use environment variable for deployment

--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -6,6 +6,7 @@ function AddUser() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [role, setRole] = useState("USER");
+  const[users,setUsers]
 
 const handleSubmit = async (e) => {
   e.preventDefault();
